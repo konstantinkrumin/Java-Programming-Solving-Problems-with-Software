6b9e2b20e62c2650e4e0086b04e3dfde28199875
Konstantin Krumin <konstantin.krumin@gmail.com>
2019-10-10 00:32:38 +0300

    Add files via upload

---
+++ Week 2/StringsFirstAssignments/Part1.java
@@ -0,0 +1,47 @@
+
+public class Part1 {
+    public String findSimpleGene (String dna) {
+        String result = "";
+        int startIndex = dna.indexOf("ATG");
+        if (startIndex == -1) {
+            return "";
+        }
+        int endIndex = dna.indexOf("TAA");
+         if (endIndex == -1) {
+            return "";
+        }
+        result = dna.substring(startIndex, endIndex + 3);
+        if (result.length() % 3 == 0) {
+            return result;
+        }
+        else {
+            return "";
+        }
+    }
+    public void testSimpleGene () {
+        String dna = "ATTGCCTAA";
+        System.out.println("First DNA strand is " + dna);
+        String gene = findSimpleGene(dna);
+        System.out.println("Gene is " + gene);
+        
+        dna = "ATGGCCTGA";
+        System.out.println("Second DNA string is " + dna);
+        gene = findSimpleGene(dna);
+        System.out.println("Gene is " + gene);
+        
+        dna = "ATCGCCTGA";
+        System.out.println("Third DNA string is " + dna);
+        gene = findSimpleGene(dna);
+        System.out.println("Gene is " + gene);
+        
+        dna = "ATGGCCTTGTAA";
+        System.out.println("Fourth DNA string is " + dna);
+        gene = findSimpleGene(dna);
+        System.out.println("Gene is " + gene);
+        
+        dna = "ATGGCCTGTAA";
+        System.out.println("Fifth DNA string is " + dna);
+        gene = findSimpleGene(dna);
+        System.out.println("Gene is " + gene);
+    }
+}
---
+++ Week 2/StringsFirstAssignments/Part4.java
@@ -0,0 +1,16 @@
+import edu.duke.*;
+import java.io.File;
+
+public class Part4 {
+    public void checkURL() {
+        URLResource resource = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+        for (String word : resource.words()) {
+            int youtubeIndex = word.toLowerCase().indexOf("youtube.com");
+            if (youtubeIndex != -1) {
+                int left = word.lastIndexOf("\"", youtubeIndex);
+                int right = word.indexOf("\"", youtubeIndex);
+                System.out.println(word.substring(left+1, right));
+            }
+        } 
+    }
+}
