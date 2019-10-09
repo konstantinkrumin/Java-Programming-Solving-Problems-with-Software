ea2f2d4ba7e2a5d0b59ff3abaac57e1975856d5a
Konstantin Krumin <konstantin.krumin@gmail.com>
2019-10-10 00:34:44 +0300

    Add files via upload

---
+++ Week 2/StringsSecondAssignments/Part2.java
@@ -0,0 +1,38 @@
+
+public class Part2 {
+    public int howMany (String stringa, String stringb) {
+        int countOccurrencies = 0;
+        int firstOccurrence = stringb.indexOf(stringa);
+        if (firstOccurrence != -1) {
+            countOccurrencies += 1;
+            while (stringb.indexOf(stringa, firstOccurrence) != -1 && firstOccurrence != -1) {
+               countOccurrencies += 1;
+               firstOccurrence = stringb.indexOf(stringa, firstOccurrence + stringa.length());
+            }
+            countOccurrencies -= 1;
+        }
+        else {
+            countOccurrencies = 0;
+        }
+        return countOccurrencies;
+    }
+    public void testHowMany() {
+        String stringa = "ATG";
+        String stringb = "ATTATGGCCATGTAA";
+        System.out.println("First DNA strand is " + stringb);
+        int count = howMany(stringa, stringb);
+        System.out.println("There are " + count + " occurencies of " + stringa + " in " + stringb);
+        
+        stringa = "TT";
+        stringb = "ATTTATTAATT";
+        System.out.println("First DNA strand is " + stringb);
+        count = howMany(stringa, stringb);
+        System.out.println("There are " + count + " occurencies of " + stringa + " in " + stringb);
+        
+        stringa = "AA";
+        stringb = "ATTTATTAATT";
+        System.out.println("First DNA strand is " + stringb);
+        count = howMany(stringa, stringb);
+        System.out.println("There are " + count + " occurencies of " + stringa + " in " + stringb);
+    }
+}
---
+++ Week 2/StringsSecondAssignments/Part3.java
@@ -0,0 +1,61 @@
+
+public class Part3 {
+    public int findStopCodon(String dna, int startIndex, String stopCodon) {
+        int currIndex = dna.indexOf(stopCodon, startIndex);
+        while (currIndex != -1) {
+            int difference = (currIndex - startIndex) % 3;
+            if (difference == 0) {
+                return currIndex;
+            }
+            else {
+                currIndex = dna.indexOf(stopCodon, currIndex + 1);
+            }
+        }
+        return dna.length();
+    }
+    public String findGene(String dna, int where) {
+        int startIndex = dna.indexOf("ATG", where);
+        if (startIndex == -1) {
+            return "";
+        }
+        int taaIndex = findStopCodon(dna, startIndex, "TAA");
+        int tagIndex = findStopCodon(dna, startIndex, "TAG");
+        int tgaIndex = findStopCodon(dna, startIndex, "TGA");
+        
+        int temp = Math.min(taaIndex, tagIndex);
+        int minIndex = Math.min(temp, tgaIndex);
+        if (minIndex == dna.length()) {
+            return "";
+        }
+        
+        
+        return dna.substring(startIndex, minIndex + 3);
+    }
+    public void printAllGenes(String dna) {
+        int startIndex = 0;
+        while (true) {
+            String currentGene = findGene(dna, startIndex);
+            if (currentGene.isEmpty()) {
+                break;
+            }
+            System.out.println(currentGene);
+            startIndex = dna.indexOf(currentGene, startIndex) + currentGene.length();
+        }            
+    }
+    public void countGenes(String dna) {
+        int startIndex = 0;
+        while (true) {
+            String currentGene = findGene(dna, startIndex);
+            if (currentGene.isEmpty()) {
+                break;
+            }
+            System.out.println("Numbers of genes found:" + currentGene);
+            startIndex = dna.indexOf(currentGene, startIndex) + currentGene.length();
+        }
+    }
+    public void testCountGenes() {
+        String dna = "ATGCCCTAACCCATGCCCCCCTGACC";
+        System.out.println("The first DNA strand is:" + dna);
+        countGenes(dna);
+    }
+}
