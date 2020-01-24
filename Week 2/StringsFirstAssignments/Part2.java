ff703b20803decd656d56ec2cd38490c74d0cd4e
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-01-25 02:35:42 +0300

    Add files via upload

---
+++ Week 2/StringsFirstAssignments/Part2.java
@@ -0,0 +1,68 @@
+
+/**
+ * findSimpleGene method finds genes in DNA strands.
+ * 
+ * Author: Konstantin Krumin
+ * Version: 1.0
+ */
+
+public class Part2 {
+    public String findSimpleGene (String dna, String startCodon, String endCodon) {
+        String result = "";
+        
+        if (Character.isUpperCase(dna.charAt(0))) {
+            startCodon = startCodon.toUpperCase();
+            endCodon = endCodon.toUpperCase();
+        }
+        else {
+            startCodon = startCodon.toLowerCase();
+            endCodon = endCodon.toLowerCase();
+        }
+        
+        int startIndex = dna.indexOf(startCodon);
+        if (startIndex == -1) {
+            return "";
+        }
+        
+        int endIndex = dna.indexOf(endCodon);
+         if (endIndex == -1) {
+            return "";
+        }
+        
+        result = dna.substring(startIndex, endIndex + 3);
+        if (result.length() % 3 == 0) {
+            return result;
+        }
+        else {
+            return "";
+        }
+        
+    }
+    
+    public void testSimpleGene () {
+        String dna = "ATTGCCTAA";
+        System.out.println("First DNA strand is " + dna);
+        String gene = findSimpleGene(dna, "ATG", "TAA");
+        System.out.println("Gene is " + gene);
+        
+        dna = "ATGGCCTGA";
+        System.out.println("Second DNA string is " + dna);
+        gene = findSimpleGene(dna, "ATG", "TAA");
+        System.out.println("Gene is " + gene);
+        
+        dna = "ATCGCCTGA";
+        System.out.println("Third DNA string is " + dna);
+        gene = findSimpleGene(dna, "ATG", "TAA");
+        System.out.println("Gene is " + gene);
+        
+        dna = "AAAATGGCCTTGTAAGAA";
+        System.out.println("Fourth DNA string is " + dna);
+        gene = findSimpleGene(dna, "ATG", "TAA");
+        System.out.println("Gene is " + gene);
+        
+        dna = "aacatggccttgtaattg";
+        System.out.println("Fifth DNA string is " + dna);
+        gene = findSimpleGene(dna, "ATG", "TAA");
+        System.out.println("Gene is " + gene);
+    }
+}
---
+++ Week 2/StringsFirstAssignments/Part3.java
@@ -0,0 +1,71 @@
+
+/**
+ * twoOccurrences method determines whether a stringb contains 2 occurencies of the stringa.
+ * 
+ * lastPart method determines whether a stringa is the last part of the stringb.
+ * 
+ * Author: Konstantin Krumin
+ * Version: 1.0
+ */
+
+public class Part3 {
+    public boolean twoOccurrences (String stringa, String stringb) {
+        
+        int occurrenceTest1 = stringb.indexOf(stringa);
+        if (occurrenceTest1 != -1) {
+            int occurrenceTest2 = stringb.indexOf(stringa, occurrenceTest1 + 1);
+            if (occurrenceTest2 != -1) {
+                return true;
+            }
+            else {
+                return false;
+            }
+            
+        }
+        else {
+            return false;
+        }
+    }
+    
+    public void testTwoOccurrences() {
+        boolean case1 = twoOccurrences("by", "A story by Abby Long");
+        System.out.println("Are there two occurrences? " + case1);
+        
+        boolean case2 = twoOccurrences("a", "banana");
+        System.out.println("Are there two occurrences? " + case2);
+        
+        boolean case3 = twoOccurrences("atg", "caatgccaatg");
+        System.out.println("Are there two occurrences? " + case3);
+        
+        boolean case4 = twoOccurrences("be", "If you wanna be strong, eat well!");
+        System.out.println("Are there two occurrences? " + case4);
+    }
+    
+    public String lastPart (String stringa, String stringb) {
+        int firstOccurrence = stringb.indexOf(stringa);
+        int stringaLength = stringa.length();
+        
+        if (firstOccurrence != -1) {
+            String restOfStringb = stringb.substring(firstOccurrence + stringaLength);
+            return restOfStringb;
+        }
+        else {
+            return stringb;
+        }
+    }
+    
+    public void testLastPart() {
+       String case1 = lastPart("ana","Banana"); 
+       System.out.println("The last part of the word is " + case1);
+       
+       String case2 = lastPart("zoo","forest"); 
+       System.out.println("The last part of the word is " + case2);
+       
+       String case3 = lastPart("atg","cggtaaatgaaaaa"); 
+       System.out.println("The last part of the word is " + case3);
+       
+       String case4 = lastPart("taa","atgcaatggga"); 
+       System.out.println("The last part of the word is " + case4);
+    }
+}
+
