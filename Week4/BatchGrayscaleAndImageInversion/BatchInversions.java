6746239118eb3bc38c6cb2a722af6027b58f5c65
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-01-25 02:49:46 +0300

    Add files via upload

--- Week4/BatchGrayscaleAndImageInversion/BatchInversions.java
+++ Week4/BatchGrayscaleAndImageInversion/BatchInversions.java
@@ -1,5 +1,11 @@
 
 /**
+ * BatchInversions class: 
+ * * makeInversion method processes an image and converts it into inverted image.
+ * * SelectMultipleConvertAndSave method selects multiple files, converts into inverted and 
+ * * * saves new files under new names.
+ * 
+ * 
  * @ Konstantin Krumin 
  * @ 1.0.0
  */
@@ -8,15 +14,11 @@ import edu.duke.*;
 import java.io.*;
 
 public class BatchInversions {
-    // makeInversion going to process an image and convert it into inverted image
     public ImageResource makeInversion (ImageResource inImage) {
-        // Create a new ImageResource outImage with same width and height as inImage
         ImageResource outImage = new ImageResource (inImage.getWidth(), inImage.getHeight());
-        // Iterate over each pixel in the outImage
+        
         for (Pixel pixel : outImage.pixels()) {
-            // Get coordinates for each iterated pixel in inImage and assign them to inPixel
             Pixel inPixel = inImage.getPixel(pixel.getX(), pixel.getY());
-            // Calculated the inverted color scheme for each pixel and assign it to the new values
             int invertedRed = 255 - inPixel.getRed();
             int invertedGreen = 255 - inPixel.getGreen();
             int invertedBlue = 255 - inPixel.getBlue();
@@ -25,19 +27,14 @@ public class BatchInversions {
             pixel.setGreen(invertedGreen);
             pixel.setBlue(invertedBlue);
         }
-        // outImage is the final answer
         return outImage;
     }
-    // SelectMultipleConvertAndSave selects multiple files, converts into inverted and saves
-    // new files under new names
+    
     public void SelectMultipleConvertAndSave () {
-        // Create a new DirectoryResource to access multiple files
         DirectoryResource dr = new DirectoryResource();
-        // Iterate over each file in the directory
+        
         for (File file : dr.selectedFiles()) {
-            // Set each file as an ImageResource
             ImageResource inImage = new ImageResource(file);
-            // Convert each ImageResource into inverted using makeInversion() method
             ImageResource invertedImage = makeInversion(inImage);
             // Get the current filename and set a new filename to "inverted-" + filename
             String fname = inImage.getFileName();
--- Week4/BatchGrayscaleAndImageInversion/GrayScaleConverter.java
+++ Week4/BatchGrayscaleAndImageInversion/GrayScaleConverter.java
@@ -1,5 +1,11 @@
 
 /**
+ * GrayScaleConverter class :
+ * * makeGray method processes an image and converts it into grayscale.
+ * * SelectMultipleConvertAndSave selects multiple files, converts into grayscale and 
+ * * * saves new files under a new name.
+ * 
+ * 
  * @ Konstantin Krumin 
  * @ 1.0.0
  */
@@ -8,34 +14,25 @@ import edu.duke.*;
 import java.io.*;
 
 public class GrayScaleConverter {
-    // makeGray going to process an image and convert it into grayscale
     public ImageResource makeGray (ImageResource inImage) {
-        // Create a new ImageResource outImage with same width and height as inImage
         ImageResource outImage = new ImageResource (inImage.getWidth(), inImage.getHeight());
-        // Iterate over each pixel in the outImage
+        
         for (Pixel pixel : outImage.pixels()) {
-            // Get coordinates for each iterated pixel in inImage and assign them to inPixel
             Pixel inPixel = inImage.getPixel(pixel.getX(), pixel.getY());
-            // Get the average values for RGB of each iterated pixel
             int average = (inPixel.getRed() + inPixel.getGreen() + inPixel.getBlue()) / 3;
             // Set each iterated pixel to average value
             pixel.setRed(average);
             pixel.setGreen(average);
             pixel.setBlue(average);
         }
-        // outImage is the final answer
         return outImage;
     }
-    // SelectMultipleConvertAndSave selects multiple files, converts into grayscale and saves
-    // new files under a new name
+    
     public void SelectMultipleConvertAndSave () {
-        // Create a new DirectoryResource to access multiple files
         DirectoryResource dr = new DirectoryResource();
-        // Iterate over each file in the directory
+        
         for (File file : dr.selectedFiles()) {
-            // Set each file as an ImageResource
             ImageResource inImage = new ImageResource(file);
-            // Convert each ImageResource into grayscale using makeGray() method
             ImageResource grayImage = makeGray(inImage);
             // Get the current filename and set a new filename to "gray-" + filename
             String fname = inImage.getFileName();
